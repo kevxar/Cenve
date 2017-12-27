885e5cfa389ddbaafc14255082841c8f4cf4c636
Jean <32343460+Eljean1@users.noreply.github.com>
2017-12-27 06:27:13 -0300

    Registros 2
    - Se agregó la columna "fechaSalida" en "Registro"
    - Validaciones al momento de Ingreso y Salida de un vehículo

--- app/src/main/java/cl/ucn/disc/dam/cenve/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dam/cenve/activities/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.UpdateBuilder;
 
 import java.sql.SQLException;
 import java.util.Calendar;
@@ -40,7 +41,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener{
 
     /**
-     * Adapter de {@Link cl.ucn.disc.dam.cenve.model.Registro}
+     * Adapter de {@Link cl.ucn.disc.dam.cenve.model.Vehiculo}
      */
     private BaseAdapter baseAdapter;
 //    private GetRegisterTask getRegisterTask;
@@ -166,6 +167,52 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
         salida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                try{
+                    Dao<Registro, String> registroDao;
+                    registroDao = getHelper().getRegistroDao();
+                    QueryBuilder<Registro, String> registroQb = registroDao.queryBuilder();
+                    UpdateBuilder<Registro, String> registroUb = registroDao.updateBuilder();
+                    registroQb.setCountOf(true);
+
+                    //Si la persona está dentro hay que registrar la fecha de salida
+                    if(registroDao.countOf(registroQb.where()
+                            .eq(Registro.PORTERIA, Porteria.NORTE.toString())
+                            .and()
+                            .eq(Registro.VEHICULO, auto.getPatente())
+                            .and()
+                            .isNull(Registro.FECHA_SALIDA)
+                            .prepare()) == 1){
+
+                        //Fecha de Salida
+                        Calendar calendar = Calendar.getInstance();
+                        Date fechaSalida =  calendar.getTime();
+
+                        //Cambiar fecha de Salida en la BAse de Datos
+                        registroUb.updateColumnValue("fechaSalida", fechaSalida);
+                        registroUb.where()
+                                .eq(Registro.PORTERIA, Porteria.NORTE.toString())
+                                .and()
+                                .eq(Registro.VEHICULO, auto.getPatente())
+                                .and()
+                                .isNull(Registro.FECHA_SALIDA);
+                        registroUb.update();
+
+                        Toast toast1 =
+                                Toast.makeText(getApplicationContext(),
+                                        "Salida Exitosa" , Toast.LENGTH_SHORT);
+                        toast1.show();
+                    }else{
+                        //Si no está dentro desplegar mensaje
+                        Toast toast2 =
+                                Toast.makeText(getApplicationContext(),
+                                        "El vehículo no ha ingresado" , Toast.LENGTH_SHORT);
+                        toast2.show();
+
+                    }
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
                 alertDialog.dismiss();
             }
         });
@@ -179,26 +226,39 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
                     Dao<Registro, String> registroDao;
                     registroDao = getHelper().getRegistroDao();
                     QueryBuilder<Registro, String> registroQb = registroDao.queryBuilder();
-
-                    Calendar calendar = Calendar.getInstance();
-                    Date fecha =  calendar.getTime();
-
-                    //Crear Registro a ingresar
-                    Registro reg = new Registro(Porteria.NORTE.toString(), fecha, auto);
-
-                    //Crear registro en la Base de Datos
-                    registroDao.create(reg);
                     registroQb.setCountOf(true);
 
-                    Toast toast1 =
-                            Toast.makeText(getApplicationContext(),
-                                    "Registros en BD: " +registroDao.countOf(registroQb.where()
-                                            .eq(Registro.PORTERIA, Porteria.NORTE.toString())
-                                            .and()
-                                            .eq(Registro.VEHICULO, auto)
-                                            .prepare())
-                                    , Toast.LENGTH_SHORT);
-                    toast1.show();
+                    //Fecha de ingreso
+                    Calendar calendar = Calendar.getInstance();
+                    Date fechaIngreso =  calendar.getTime();
+
+                    //Crear Registro a ingresar
+                    Registro reg = new Registro(Porteria.NORTE.toString(), fechaIngreso, null, auto);
+
+                    //Si el vehículo no ha ingresado, hacer el registro
+                    if(registroDao.countOf(registroQb.where()
+                            .eq(Registro.PORTERIA, Porteria.NORTE.toString())
+                            .and()
+                            .eq(Registro.VEHICULO, auto.getPatente())
+                            .and()
+                            .isNull(Registro.FECHA_SALIDA)
+                            .prepare()) == 0) {
+
+                        //Crear registro en la Base de Datos
+                        registroDao.create(reg);
+
+                        Toast toast1 =
+                                Toast.makeText(getApplicationContext(),
+                                        "Registro exitoso " , Toast.LENGTH_SHORT);
+                        toast1.show();
+                    }else{
+                        //Si el vehículo ya ha ingresado, desplegar mensaje
+                        Toast toast2 =
+                                Toast.makeText(getApplicationContext(),
+                                        "El vehículo ya se encuentra adentro" , Toast.LENGTH_SHORT);
+                        toast2.show();
+                    }
+                    alertDialog.dismiss();
 
                 }catch(SQLException e){
                     e.printStackTrace();
--- app/src/main/java/cl/ucn/disc/dam/cenve/model/Registro.java
+++ app/src/main/java/cl/ucn/disc/dam/cenve/model/Registro.java
@@ -25,15 +25,17 @@ public class Registro {
     public static final String ID = "registro_id";
     public static final String PORTERIA = "porteria";
     public static final String FECHA_INGRESO = "fechaIngreso";
+    public static final String FECHA_SALIDA = "fechaSalida";
     public static final String VEHICULO = "vehiculo";
 
     public Registro() {
 
     }
 
-    public Registro(String porteria, Date fechaIngreso, Vehiculo vehiculo) {
+    public Registro(String porteria, Date fechaIngreso, Date fechaSalida, Vehiculo vehiculo) {
         this.porteria = porteria;
         this.fechaIngreso = fechaIngreso;
+        this.fechaIngreso = fechaSalida;
         this.vehiculo = vehiculo;
     }
 
@@ -61,6 +63,14 @@ public class Registro {
     @Setter
     @DatabaseField(columnName = FECHA_INGRESO)
     private Date fechaIngreso;
+
+    /**
+     * Fecha de salida en la cual el auto sale de los estacionamientos
+     */
+    @Getter
+    @Setter
+    @DatabaseField(columnName = FECHA_SALIDA, canBeNull = true)
+    private Date fechaSalida;
 
     /**
      * Vehiculo que se esta registrando por su ingreso
